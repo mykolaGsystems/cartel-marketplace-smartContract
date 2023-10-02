492906727e285e35567c61ad5107346a38788eab
mykolaGsystems <kolangavrish0001@ukr.net>
2023-10-02 23:08:58 +0200

    Code cleaning  & formating

--- contract/src/store_op.rs
+++ contract/src/store_op.rs
@@ -54,69 +54,7 @@ impl Contract {
     // View the current state of the marketplace
     // pub fn view_store(&self) -> Vec<(String, Vec<(String, HashMap<String, f64>)>)> {
     pub fn view_store(&self) -> Vec<(String, MarketplaceItem)> {
-
-        // let mut return_store = 
-         
         self.marketplace_stock.to_vec()
-        // let mut view_items: Vec<(String, Vec<(String, HashMap<String, f64>)>)> = Vec::new();
-        
-        // for(item_id, item_params) in self.marketplace_stock.iter() {
-        // let unique_keys = self.marketplace_stock.keys_as_vector().to_vec();
-
-        // for key in unique_keys {
-        //     let element = self.marketplace_stock.get(&key).unwrap();
-        //     let items_grind = element.keys_as_vector().to_vec();
-            
-        //     for t in items_grind {
-        //         let g = element.get(&t).unwrap();
-        //         log!("{:?}", g);
-        //     };
-
-        //     log!("{}", &key);
-            
-        // }
-
-
-        // let mut b = 0.0;
-            
-            
-            // log!("id {:?} ",e);
-        // for(item_type, item_prices) in v.iter() {
-        //     // log!("ID : {:?}", item_prices);
-        //     b += item_prices.get_key_value(&"500");
-            
-
-            
-        // }
-        // }
-
-        // let vec_marketplace: Vec<(String, UnorderedMap<String, StoreItem>)> = self.marketplace_stock.to_vec();
-
-        // for items in vec_marketplace {
-
-        //     let mut single_view_item: (String, Vec<(String, HashMap<String, f64>)>) = (String::from(""), Vec::new());
-        //     let mut vec: Vec<(String, HashMap<String, f64>)> = Vec::new();
-        //     // view_items.push((item.0, item.1.to_vec()));
-        //     let item_prices: Vec<(String, StoreItem)> = items.1.to_vec();
-          
-
-        //     single_view_item.0 = items.0;
-
-        //     for item in item_prices {
-        //         log!("Current items: {}, {:?}", &item.0, &item.1.prices );
-        //         vec.push((item.0, item.1.prices));
-                
-        //     }
-
-        //     single_view_item.1 = vec;
-
-        //     view_items.push(single_view_item);
-
-        // };
-
-        // return view_items
-        // return self.marketplace_stock.keys_as_vector().to_vec();
-
     }
 
     // View the current available delivery regions 
