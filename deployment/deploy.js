d09aef5e70d3feb064bdec8f89ff3abab9e98892
mykolaGsystems <kolangavrish0001@ukr.net>
2023-10-08 00:12:49 +0200

    Added wider coverage for testing functions of smart contract

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -18,6 +18,8 @@ const connectionConfig = {
     explorerUrl: "https://explorer.testnet.near.org",
 };
 
+const encoded_message = "GAutg1WvCnxn93YHqjZ1e1Cgf3VLXc40spkkBLoTU3t0x/fAKh5t7MQbGoGMmBMhHDmu3LnZufqlltlR2KjYiWjc+JgNg40FC2aM0jMO5ao1vlSUJw7/OBKIeeYgtom2U6IXIckrPX+2XI7XOG7B6Ga3zK9cgGBOogiIClPITYUjSmCkNLX/HINOceSPpoLoj8ISCmB8dG0gMJe0cFLKqcv9z8KNvdvMIoe3xa77MWsH+WlAdRswJCCqfWE6zz0Aj6B649tSX7S/U8L1ScQ+3c+y0Z6N5iBbv3CF6vuWmwMU=U";
+
 const delivery_options = [
     {
         "region" : "AS",
@@ -77,17 +79,17 @@ const items = [
  
 (async () => {
     const nearConnection = await connect(connectionConfig);
-    const marketplaceAccount = await nearConnection.account("m20.hugebobadev.testnet");
-    const masterAccount = "m20.hugebobadev.testnet"
+    const marketplaceAccount = await nearConnection.account("m21.hugebobadev.testnet");
+    const masterAccount = "m21.hugebobadev.testnet"
 
     //Deploy && Setup Contract
 
 	await marketplaceAccount.deployContract(fs.readFileSync("../output_wasm/marketplace.wasm"));
-	console.log("Marketplace contract deployed");
+	console.log("[INFO]  Marketplace contract deployed");
 
     const marketplaceContract = new Contract(marketplaceAccount, masterAccount, {
-		viewMethods: ["view_store", "view_delivery_rates"],
-		changeMethods: ["insert_marketplace_item", "insert_delivery_regions", "reset_store"],
+		viewMethods: ["view_store", "view_delivery_rates", "view_fulfilled_orders", "view_pending_orders"],
+		changeMethods: ["insert_marketplace_item", "insert_delivery_regions", "confirm_purchase", "confirm_order" ],
 	});
 
     for (let i = 0; i< delivery_options.length; i++) {
@@ -103,14 +105,53 @@ const items = [
                 } 
             }
         );
-        console.log("Marketplace delivery region deployed: ", delivery_options[i].region);
+        console.log("[TEMP] Marketplace delivery region deployed: ", delivery_options[i].region);
     };
 
-    console.log("\n Delivery rates has been uploaded! Starting loading the items to the stock... \n");
+    console.log("\n[INFO]  Delivery rates has been uploaded! Starting loading the items to the stock... \n");
 
     for( let i = 0; i < items.length; i++)  {
         await marketplaceContract.insert_marketplace_item({ args: { item_id : items[i].id, item_name: items[i].name, grinds: items[i].grinds, price: items[i].price } });
-        console.log("Marketplace insert of item : ", items[i].name);
+        console.log("[TEMP] Marketplace insert of item : ", items[i].name);
     }
+
+    console.log("\n[INFO] Items has been uploaded! \n");
+    console.log("\n[INFO] Performing validations of smart contract");
+
+    console.log("\n[INFO] Executing the purchase transaction!")
+
+    await marketplaceContract.confirm_purchase({ 
+        args : {
+            "encoded_message" : encoded_message,
+            "items": [
+                [
+                "fd324893-1880-4a85-afd0-e5be598aac7c_250_Espresso",
+                1
+                ]
+            ]
+        },  
+        gas: "250000000000000",
+        amount: nearAPI.utils.format.parseNearAmount("22.5")      
+    })
+
+    // console.log("\n[INFO] Checking if the transaction was successful!");
+    let pending_order = await marketplaceContract.view_pending_orders();
+    // console.log("Pending order: ", pending_order);
+    let pending_order_id = pending_order[0][0]
+    console.log("\n[INFO] Checking if the order fulfillment was successful!");
+
+    await marketplaceContract.confirm_order({ 
+        args : {
+            "order_id" : pending_order_id,
+            "order_filled" : true,
+        },  
+        gas: "250000000000000", 
+    })
+
+    let pending_order_updated = await marketplaceContract.view_pending_orders();
+    console.log("\n[INFO] Pending order updated Status: ", pending_order_updated)
+    let fulfilled_order = await marketplaceContract.view_fulfilled_orders()
+    console.log("\n[INFO] Fulfilled order updated Status: ", fulfilled_order.length, fulfilled_order)
+
     
 })();
